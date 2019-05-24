414e7803fc1ba1f54f63f1e6c54632dcac0a595d
LeviSylv <levisylv@amazon.com>
2019-05-24 13:12:28 -0700

    subscribed to quizzes and ngfor quiz loop

--- client/final-client/src/app/dashboard/dashboard.component.ts
+++ client/final-client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../quiz.service'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+  quizzes;
+  user = JSON.parse(localStorage.getItem('currentUser'));
+  constructor(private quiz_api: QuizService) {
+    this.getquizzes();
+  }
+    
   ngOnInit() {
   }
-
+  getquizzes(){
+    this.quiz_api.getAllQuizzes(this.user.id).subscribe(
+      data => {
+        this.quizzes = data;
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
 }
