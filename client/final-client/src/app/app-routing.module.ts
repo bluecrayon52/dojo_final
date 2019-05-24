82621172a7df7b2c1df9875bb8cd009255cc8305
LeviSylv <levisylv@amazon.com>
2019-05-24 09:11:04 -0700

    routing angular p1

--- client/final-client/src/app/app-routing.module.ts
+++ client/final-client/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginRegComponent } from './login-reg/login-reg.component';
+import { NewquizComponent } from './newquiz/newquiz.component';
+import { QuestionComponent } from './question/question.component';
+import { AddquestionComponent } from './addquestion/addquestion.component';
+import { DisplayquestionComponent } from './displayquestion/displayquestion.component';
+import { YourscoreComponent } from './yourscore/yourscore.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login-reg', component:LoginRegComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'newquiz', component:NewquizComponent},
+  {path: 'question', component:QuestionComponent},
+  {path: 'addquestion', component:AddquestionComponent},
+  {path: 'yourquiz', component:DisplayquestionComponent},
+  {path: 'yourscore', component:YourscoreComponent},
+  {path: 'play', component:QuestionComponent},
+  {path: 'edit', component:DisplayquestionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
